bca65fa9bf3651f12e6d9ca67177456e1ea846a2
j_b0gie <wxzvrd@protonmail.com>
2023-08-20 21:47:03 +0100

    let the tweaking begin!

---
+++ common_md/schwepe.md
@@ -0,0 +1,9 @@
+---
+title: schwepe
+date: now
+---
+
+![](https://247420.xyz/random-png)
+
+# This is Schwepe 
+	Protector of jeeves
--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,14 +3,14 @@ import { DefaultSeoProps } from "next-seo";
 const description = "Host your second brain with NextJS"
 // See https://www.npmjs.com/package/next-seo for more options
 const config: DefaultSeoProps = {
-  titleTemplate: "%s | Linked Blog Starter",
-  defaultTitle: "Linked Blog Starter | My Second Brain",
+  titleTemplate: '%s | hikarijournals',
+  defaultTitle: 'hikarijournals',
   description,
 
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    title: 'Linked Blog Starter',
+    title: 'hikarijournals',
     description,
   },
   additionalLinkTags: [
@@ -18,29 +18,29 @@ const config: DefaultSeoProps = {
       rel: 'icon',
       type: 'image/png',
       href: '/favicon/favicon-32x32.png',
-      sizes: '32x32'
+      sizes: '32x32',
     },
     {
       rel: 'icon',
       type: 'image/png',
       href: '/favicon/favicon-16x16.png',
-      sizes: '16x16'
+      sizes: '16x16',
     },
     {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      href: "/favicon/apple-touch-icon.png"
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicon/apple-touch-icon.png',
     },
     {
-      rel: "shortcut icon",
-      type: "image/png",
-      href: "/favicon/favicon.ico"
+      rel: 'shortcut icon',
+      type: 'image/png',
+      href: '/favicon/favicon.ico',
     },
     {
-      rel: "manifest",
-      href: "/favicon/site.webmanifest"
-    }
-  ]
+      rel: 'manifest',
+      href: '/favicon/site.webmanifest',
+    },
+  ],
 };
 
 export default config;
